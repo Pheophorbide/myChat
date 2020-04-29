7f6f8c3f503c667c01606938d122b0959cd34e0b
daria <daria.paramonova@gmail.com>
2020-04-29 17:21:51 +0300

    styles fixed

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -1,5 +1,5 @@
 export const required = value => (value ? '' : 'Поле не может быть пустым!');
 
 export const maxLength = value => value && value.length > 32 ? 'Имя не должно быть длиннее 32 символов!' : '';
-export const maxMsgLength = value => value && value.length > 225 ? 'Сообщение слишком длинное!' : '';
+export const maxMsgLength = value => value && value.length > 255 ? 'Сообщение слишком длинное!' : '';
 
